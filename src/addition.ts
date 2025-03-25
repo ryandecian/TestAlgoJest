02ef36cd2f3d398140ede52ebc7ab230e9f1a842
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-25 13:59:55 +0100

    Création de la fonction addition

--- src/addition.ts
+++ src/addition.ts
@@ -1,13 +1,5 @@
-type Addition = {
-    a: number;
-    b: number;
-}
 
-const tab: Addition = {
-    a: 5,
-    b: 6
-}
 
-function addition (a: number, b: number) {
+export function addition (a: number, b: number) {
     return (a + b);
 }
